002711dd788929ef0281b5ae87429cfa5e5b140f
Phildo <phildo211@gmail.com>
2015-08-24 11:45:26 -0500

    tons of attempted matrix/vector math

--- src/do_math.h
+++ src/do_math.h
@@ -3,67 +3,380 @@
 
 #include <stdlib.h>
 
+
 float randf()
 {
   return (rand()%256)/256.0f;
 }
 
-typedef struct { float x; float y; } fv2;
-typedef struct { float x; float y; float z; } fv3;
-typedef struct { float x; float y; float z; float w; } fv4;
-typedef struct { double x; double y; } dv2;
-typedef struct { double x; double y; double z; } dv3;
+typedef struct { float  x; float  y; }                     fv2;
+typedef struct { double x; double y; }                     dv2;
+typedef struct { float  x; float  y; float  z; }           fv3;
+typedef struct { double x; double y; double z; }           dv3;
 typedef struct { double x; double y; double z; double w; } dv4;
+typedef struct { float  x; float  y; float  z; float  w; } fv4;
 
-float lerpf(float s, float e, float t) { return s+(t*(e-s)); }
-double lerpd(double s, double e, double t) { return s+(t*(e-s)); }
-fv2 lerpfv2(fv2 s, fv2 e, float t)
+float  lensqrfv2(fv2 v) { return (v.x*v.x) + (v.y*v.y); }
+double lensqrdv2(dv2 v) { return (v.x*v.x) + (v.y*v.y); }
+float  lensqrfv3(fv3 v) { return (v.x*v.x) + (v.y*v.y) + (v.z*v.z); }
+double lensqrdv3(dv3 v) { return (v.x*v.x) + (v.y*v.y) + (v.z*v.z); }
+float  lensqrfv4(fv4 v) { return (v.x*v.x) + (v.y*v.y) + (v.z*v.z) + (v.w*v.w); }
+double lensqrdv4(dv4 v) { return (v.x*v.x) + (v.y*v.y) + (v.z*v.z) + (v.w*v.w); }
+
+float  lenfv2(fv2 v) { return sqrt((v.x*v.x) + (v.y*v.y)); }
+double lendv2(dv2 v) { return sqrt((v.x*v.x) + (v.y*v.y)); }
+float  lenfv3(fv3 v) { return sqrt((v.x*v.x) + (v.y*v.y) + (v.z*v.z)); }
+double lendv3(dv3 v) { return sqrt((v.x*v.x) + (v.y*v.y) + (v.z*v.z)); }
+float  lenfv4(fv4 v) { return sqrt((v.x*v.x) + (v.y*v.y) + (v.z*v.z) + (v.w*v.w)); }
+double lendv4(dv4 v) { return sqrt((v.x*v.x) + (v.y*v.y) + (v.z*v.z) + (v.w*v.w)); }
+
+fv2 normfv2(fv2 v) { float l = lenfv2(v); return fv2{ v.x/l, v.y/l };  }
+dv2 normdv2(dv2 v) { float l = lendv2(v); return dv2{ v.x/l, v.y/l };  }
+fv3 normfv3(fv3 v) { float l = lenfv3(v); return fv3{ v.x/l, v.y/l, v.z/l };  }
+dv3 normdv3(dv3 v) { float l = lendv3(v); return dv3{ v.x/l, v.y/l, v.z/l };  }
+fv4 normfv4(fv4 v) { float l = lenfv4(v); return fv4{ v.x/l, v.y/l, v.z/l, v.w/l };  }
+dv4 normdv4(dv4 v) { float l = lendv4(v); return dv4{ v.x/l, v.y/l, v.z/l, v.w/l };  }
+
+#define LERP(s,e,t) (s+(t*(e-s)))
+float  lerpf(float  s, float  e, float  t) { return LERP(s,e,t); }
+double lerpd(double s, double e, double t) { return LERP(s,e,t); }
+fv2 lerpfv2(fv2 s, fv2 e, float  t) { return fv2{ LERP(s.x,e.x,t), LERP(s.y,e.y,t) }; }
+dv2 lerpdv2(dv2 s, dv2 e, double t) { return dv2{ LERP(s.x,e.x,t), LERP(s.y,e.y,t) }; }
+fv3 lerpfv3(fv3 s, fv3 e, float  t) { return fv3{ LERP(s.x,e.x,t), LERP(s.y,e.y,t), LERP(s.z,e.z,t) }; }
+dv3 lerpdv3(dv3 s, dv3 e, double t) { return dv3{ LERP(s.x,e.x,t), LERP(s.y,e.y,t), LERP(s.z,e.z,t) }; }
+fv4 lerpfv4(fv4 s, fv4 e, float  t) { return fv4{ LERP(s.x,e.x,t), LERP(s.y,e.y,t), LERP(s.z,e.z,t), LERP(s.w,e.w,t) }; }
+dv4 lerpdv4(dv4 s, dv4 e, double t) { return dv4{ LERP(s.x,e.x,t), LERP(s.y,e.y,t), LERP(s.z,e.z,t), LERP(s.w,e.w,t) }; }
+#undef LERP
+
+fv2 mulfv2(fv2 v, float  o) { return fv2 { v.x*o, v.y*o }; }
+dv2 muldv2(dv2 v, double o) { return dv2 { v.x*o, v.y*o }; }
+fv3 mulfv3(fv3 v, float  o) { return fv3 { v.x*o, v.y*o, v.z*o }; }
+dv3 muldv3(dv3 v, double o) { return dv3 { v.x*o, v.y*o, v.z*o }; }
+fv4 mulfv4(fv4 v, float  o) { return fv4 { v.x*o, v.y*o, v.z*o, v.w*o }; }
+dv4 muldv4(dv4 v, double o) { return dv4 { v.x*o, v.y*o, v.z*o, v.w*o }; }
+
+fv2 divfv2(fv2 v, float  o) { return fv2 { v.x/o, v.y/o }; }
+dv2 divdv2(dv2 v, double o) { return dv2 { v.x/o, v.y/o }; }
+fv3 divfv3(fv3 v, float  o) { return fv3 { v.x/o, v.y/o, v.z/o }; }
+dv3 divdv3(dv3 v, double o) { return dv3 { v.x/o, v.y/o, v.z/o }; }
+fv4 divfv4(fv4 v, float  o) { return fv4 { v.x/o, v.y/o, v.z/o, v.w/o }; }
+dv4 divdv4(dv4 v, double o) { return dv4 { v.x/o, v.y/o, v.z/o, v.w/o }; }
+
+fv2 addfv2(fv2 a, fv2 b) { return fv2 { a.x+b.x, a.y+b.y }; }
+dv2 adddv2(dv2 a, dv2 b) { return dv2 { a.x+b.x, a.y+b.y }; }
+fv3 addfv3(fv3 a, fv3 b) { return fv3 { a.x+b.x, a.y+b.y, a.z+b.z }; }
+dv3 adddv3(dv3 a, dv3 b) { return dv3 { a.x+b.x, a.y+b.y, a.z+b.z }; }
+fv4 addfv4(fv4 a, fv4 b) { return fv4 { a.x+b.x, a.y+b.y, a.z+b.z, a.w+b.w }; }
+dv4 adddv4(dv4 a, dv4 b) { return dv4 { a.x+b.x, a.y+b.y, a.z+b.z, a.w+b.w }; }
+
+fv2 subfv2(fv2 a, fv2 b) { return fv2 { a.x-b.x, a.y-b.y }; }
+dv2 subdv2(dv2 a, dv2 b) { return dv2 { a.x-b.x, a.y-b.y }; }
+fv3 subfv3(fv3 a, fv3 b) { return fv3 { a.x-b.x, a.y-b.y, a.z-b.z }; }
+dv3 subdv3(dv3 a, dv3 b) { return dv3 { a.x-b.x, a.y-b.y, a.z-b.z }; }
+fv4 subfv4(fv4 a, fv4 b) { return fv4 { a.x-b.x, a.y-b.y, a.z-b.z, a.w-b.w }; }
+dv4 subdv4(dv4 a, dv4 b) { return dv4 { a.x-b.x, a.y-b.y, a.z-b.z, a.w-b.w }; }
+
+float  dotfv2(fv2 a, fv2 b) { return (a.x*b.x) + (a.y*b.y); }
+double dotdv2(dv2 a, dv2 b) { return (a.x*b.x) + (a.y*b.y); }
+float  dotfv3(fv3 a, fv3 b) { return (a.x*b.x) + (a.y*b.y) + (a.z*b.z); }
+double dotdv3(dv3 a, dv3 b) { return (a.x*b.x) + (a.y*b.y) + (a.z*b.z); }
+float  dotfv4(fv4 a, fv4 b) { return (a.x*b.x) + (a.y*b.y) + (a.z*b.z) + (a.w*b.w); }
+double dotdv4(dv4 a, dv4 b) { return (a.x*b.x) + (a.y*b.y) + (a.z*b.z) + (a.w*b.w); }
+
+fv3 crossfv3(fv3 a, fv3 b) { return fv3 { (a.y*b.z)-(a.z*b.y), (a.z*b.x)-(a.x*b.z), (a.x*b.y)-(a.y*b.x) }; }
+dv3 crossdv3(dv3 a, dv3 b) { return dv3 { (a.y*b.z)-(a.z*b.y), (a.z*b.x)-(a.x*b.z), (a.x*b.y)-(a.y*b.x) }; }
+
+typedef struct { float  x[2]; float  y[2]; } fm2;
+typedef struct { double x[2]; double y[2]; } dm2;
+typedef struct { float  x[3]; float  y[3]; float  z[3]; } fm3;
+typedef struct { double x[3]; double y[3]; double z[3]; } dm3;
+typedef struct { float  x[4]; float  y[4]; float  z[4]; float  w[4]; } fm4;
+typedef struct { double x[4]; double y[4]; double z[4]; double w[4]; } dm4;
+
+fm3 promotefm2(fm2 m)
 {
-  return fv2{
-    s.x+(t*(e.x-s.x)),
-    s.y+(t*(e.y-s.y))
-  };
+  return fm3
+    { { m.x[0], m.x[1], 0.f },
+      { m.y[0], m.y[1], 0.f },
+      {    0.f,    0.f, 1.f } };
 }
-dv2 lerpdv2(dv2 s, dv2 e, double t)
+dm3 promotedm2(dm2 m)
 {
-  return dv2{
-    s.x+(t*(e.x-s.x)),
-    s.y+(t*(e.y-s.y))
-  };
+  return dm3
+    { { m.x[0], m.x[1], 0.  },
+      { m.y[0], m.y[1], 0.  },
+      {    0. ,    0. , 1.  } };
 }
-fv3 lerpfv3(fv3 s, fv3 e, float t)
+fm4 promotefm3(fm3 m)
 {
-  return fv3{
-    s.x+(t*(e.x-s.x)),
-    s.y+(t*(e.y-s.y)),
-    s.z+(t*(e.z-s.z))
-  };
+  return fm4
+    { { m.x[0], m.x[1], m.x[2], 0.f },
+      { m.y[0], m.y[1], m.y[2], 0.f },
+      { m.z[0], m.z[1], m.z[2], 0.f },
+      {    0.f,    0.f,    0.f, 1.f } };
 }
-dv3 lerpdv3(dv3 s, dv3 e, double t)
+dm4 promotedm3(dm3 m)
 {
-  return dv3{
-    s.x+(t*(e.x-s.x)),
-    s.y+(t*(e.y-s.y)),
-    s.z+(t*(e.z-s.z))
-  };
+  return dm4
+    { { m.x[0], m.x[1], m.x[2], 0.  },
+      { m.y[0], m.y[1], m.y[2], 0.  },
+      { m.z[0], m.z[1], m.z[2], 0.  },
+      {    0. ,    0. ,    0. , 1.  } };
 }
-fv4 lerpfv4(fv4 s, fv4 e, float t)
+
+fm2 mulfm2(fm2 a, fm2 b)
 {
-  return fv4{
-    s.x+(t*(e.x-s.x)),
-    s.y+(t*(e.y-s.y)),
-    s.z+(t*(e.z-s.z)),
-    s.w+(t*(e.w-s.w))
-  };
+  return fm2
+    { { a.x[0]*b.x[0]+a.x[1]*b.y[0], a.x[0]*b.x[1]+a.x[1]*b.y[1] },
+      { a.y[0]*b.x[0]+a.y[1]*b.y[0], a.y[0]*b.x[1]+a.y[1]*b.y[1] } };
 }
-dv4 lerpdv4(dv4 s, dv4 e, double t)
+dm2 muldm2(dm2 a, dm2 b)
 {
-  return dv4{
-    s.x+(t*(e.x-s.x)),
-    s.y+(t*(e.y-s.y)),
-    s.z+(t*(e.z-s.z)),
-    s.w+(t*(e.w-s.w))
-  };
+  return dm2
+    { { a.x[0]*b.x[0]+a.x[1]*b.y[0], a.x[0]*b.x[1]+a.x[1]*b.y[1] },
+      { a.y[0]*b.x[0]+a.y[1]*b.y[0], a.y[0]*b.x[1]+a.y[1]*b.y[1] } };
+}
+fm3 mulfm3(fm3 a, fm3 b)
+{
+  return fm3
+    { { a.x[0]*b.x[0]+a.x[1]*b.y[0]+a.x[2]*b.z[0], a.x[0]*b.x[1]+a.x[1]*b.y[1]+a.x[2]*b.z[1], a.x[0]*b.x[2]+a.x[1]*b.y[2]+a.x[2]*b.z[2] },
+      { a.y[0]*b.x[0]+a.y[1]*b.y[0]+a.y[2]*b.z[0], a.y[0]*b.x[1]+a.y[1]*b.y[1]+a.y[2]*b.z[1], a.y[0]*b.x[2]+a.y[1]*b.y[2]+a.y[2]*b.z[2] },
+      { a.z[0]*b.x[0]+a.z[1]*b.y[0]+a.z[2]*b.z[0], a.z[0]*b.x[1]+a.z[1]*b.y[1]+a.z[2]*b.z[1], a.z[0]*b.x[2]+a.z[1]*b.y[2]+a.z[2]*b.z[2] } };
+}
+dm3 muldm3(dm3 a, dm3 b)
+{
+  return dm3
+    { { a.x[0]*b.x[0]+a.x[1]*b.y[0]+a.x[2]*b.z[0], a.x[0]*b.x[1]+a.x[1]*b.y[1]+a.x[2]*b.z[1], a.x[0]*b.x[2]+a.x[1]*b.y[2]+a.x[2]*b.z[2] },
+      { a.y[0]*b.x[0]+a.y[1]*b.y[0]+a.y[2]*b.z[0], a.y[0]*b.x[1]+a.y[1]*b.y[1]+a.y[2]*b.z[1], a.y[0]*b.x[2]+a.y[1]*b.y[2]+a.y[2]*b.z[2] },
+      { a.z[0]*b.x[0]+a.z[1]*b.y[0]+a.z[2]*b.z[0], a.z[0]*b.x[1]+a.z[1]*b.y[1]+a.z[2]*b.z[1], a.z[0]*b.x[2]+a.z[1]*b.y[2]+a.z[2]*b.z[2] } };
+}
+fm4 mulfm4(fm4 a, fm4 b)
+{
+  return fm4
+    { { a.x[0]*b.x[0]+a.x[1]*b.y[0]+a.x[2]*b.z[0]+a.x[3]*b.w[0], a.x[0]*b.x[1]+a.x[1]*b.y[1]+a.x[2]*b.z[1]+a.x[3]*b.w[1], a.x[0]*b.x[2]+a.x[1]*b.y[2]+a.x[2]*b.z[2]+a.x[3]*b.w[2], a.x[0]*b.x[3]+a.x[1]*b.y[3]+a.x[2]*b.z[3]+a.x[3]*b.w[3] },
+      { a.y[0]*b.x[0]+a.y[1]*b.y[0]+a.y[2]*b.z[0]+a.y[3]*b.w[0], a.y[0]*b.x[1]+a.y[1]*b.y[1]+a.y[2]*b.z[1]+a.y[3]*b.w[1], a.y[0]*b.x[2]+a.y[1]*b.y[2]+a.y[2]*b.z[2]+a.y[3]*b.w[2], a.y[0]*b.x[3]+a.y[1]*b.y[3]+a.y[2]*b.z[3]+a.y[3]*b.w[3] },
+      { a.z[0]*b.x[0]+a.z[1]*b.y[0]+a.z[2]*b.z[0]+a.z[3]*b.w[0], a.z[0]*b.x[1]+a.z[1]*b.y[1]+a.z[2]*b.z[1]+a.z[3]*b.w[1], a.z[0]*b.x[2]+a.z[1]*b.y[2]+a.z[2]*b.z[2]+a.z[3]*b.w[2], a.z[0]*b.x[3]+a.z[1]*b.y[3]+a.z[2]*b.z[3]+a.z[3]*b.w[3] },
+      { a.w[0]*b.x[0]+a.w[1]*b.y[0]+a.w[2]*b.z[0]+a.w[3]*b.w[0], a.w[0]*b.x[1]+a.w[1]*b.y[1]+a.w[2]*b.z[1]+a.w[3]*b.w[1], a.w[0]*b.x[2]+a.w[1]*b.y[2]+a.w[2]*b.z[2]+a.w[3]*b.w[2], a.w[0]*b.x[3]+a.w[1]*b.y[3]+a.w[2]*b.z[3]+a.w[3]*b.w[3] } };
+}
+dm4 muldm4(dm4 a, dm4 b)
+{
+  return dm4
+    { { a.x[0]*b.x[0]+a.x[1]*b.y[0]+a.x[2]*b.z[0]+a.x[3]*b.w[0], a.x[0]*b.x[1]+a.x[1]*b.y[1]+a.x[2]*b.z[1]+a.x[3]*b.w[1], a.x[0]*b.x[2]+a.x[1]*b.y[2]+a.x[2]*b.z[2]+a.x[3]*b.w[2], a.x[0]*b.x[3]+a.x[1]*b.y[3]+a.x[2]*b.z[3]+a.x[3]*b.w[3] },
+      { a.y[0]*b.x[0]+a.y[1]*b.y[0]+a.y[2]*b.z[0]+a.y[3]*b.w[0], a.y[0]*b.x[1]+a.y[1]*b.y[1]+a.y[2]*b.z[1]+a.y[3]*b.w[1], a.y[0]*b.x[2]+a.y[1]*b.y[2]+a.y[2]*b.z[2]+a.y[3]*b.w[2], a.y[0]*b.x[3]+a.y[1]*b.y[3]+a.y[2]*b.z[3]+a.y[3]*b.w[3] },
+      { a.z[0]*b.x[0]+a.z[1]*b.y[0]+a.z[2]*b.z[0]+a.z[3]*b.w[0], a.z[0]*b.x[1]+a.z[1]*b.y[1]+a.z[2]*b.z[1]+a.z[3]*b.w[1], a.z[0]*b.x[2]+a.z[1]*b.y[2]+a.z[2]*b.z[2]+a.z[3]*b.w[2], a.z[0]*b.x[3]+a.z[1]*b.y[3]+a.z[2]*b.z[3]+a.z[3]*b.w[3] },
+      { a.w[0]*b.x[0]+a.w[1]*b.y[0]+a.w[2]*b.z[0]+a.w[3]*b.w[0], a.w[0]*b.x[1]+a.w[1]*b.y[1]+a.w[2]*b.z[1]+a.w[3]*b.w[1], a.w[0]*b.x[2]+a.w[1]*b.y[2]+a.w[2]*b.z[2]+a.w[3]*b.w[2], a.w[0]*b.x[3]+a.w[1]*b.y[3]+a.w[2]*b.z[3]+a.w[3]*b.w[3] } };
+}
+
+fm2 identityfm2()
+{
+  return fm2
+    { { 1.f, 0.f },
+      { 0.f, 1.f } };
+}
+dm2 identitydm2()
+{
+  return dm2
+    { { 1. , 0.  },
+      { 0. , 1.  } };
+}
+fm3 identityfm3()
+{
+  return fm3
+    { { 1.f, 0.f, 0.f },
+      { 0.f, 1.f, 0.f },
+      { 0.f, 0.f, 1.f } };
+}
+dm3 identitydm3()
+{
+  return dm3
+    { { 1. , 0. , 0.  },
+      { 0. , 1. , 0.  },
+      { 0. , 0. , 1.  } };
+}
+fm4 identityfm4()
+{
+  return fm4
+    { { 1.f, 0.f, 0.f, 0.f },
+      { 0.f, 1.f, 0.f, 0.f },
+      { 0.f, 0.f, 1.f, 0.f },
+      { 0.f, 0.f, 0.f, 1.f } };
+}
+dm4 identitydm4()
+{
+  return dm4
+    { { 1. , 0. , 0. , 0.  },
+      { 0. , 1. , 0. , 0.  },
+      { 0. , 0. , 1. , 0.  },
+      { 0. , 0. , 0. , 1.  } };
+}
+
+fm2 translatefm2(fm2 m, fv2 v)
+{
+  return fm2
+    { { m.x[0], m.x[1]+v.x },
+      { m.y[0], m.y[1]+v.y } };
+}
+dm2 translatedm2(dm2 m, dv2 v)
+{
+  return dm2
+    { { m.x[0], m.x[1]+v.x },
+      { m.y[0], m.y[1]+v.y } };
+}
+fm3 translatefm3(fm3 m, fv3 v)
+{
+  return fm3
+    { { m.x[0], m.x[1], m.x[2]+v.x },
+      { m.y[0], m.y[1], m.y[2]+v.y },
+      { m.z[0], m.z[1], m.z[2]+v.z } };
+}
+dm3 translatedm3(dm3 m, dv3 v)
+{
+  return dm3
+    { { m.x[0], m.x[1], m.x[2]+v.x },
+      { m.y[0], m.y[1], m.y[2]+v.y },
+      { m.z[0], m.z[1], m.z[2]+v.z } };
+}
+fm4 translatefm4(fm4 m, fv4 v)
+{
+  return fm4
+    { { m.x[0], m.x[1], m.x[2], m.x[3]+v.x },
+      { m.y[0], m.y[1], m.y[2], m.y[3]+v.y },
+      { m.z[0], m.z[1], m.z[2], m.z[3]+v.z },
+      { m.w[0], m.w[1], m.w[2], m.w[3]+v.w } };
+}
+dm4 translatedm4(dm4 m, dv4 v)
+{
+  return dm4
+    { { m.x[0], m.x[1], m.x[2], m.x[3]+v.x },
+      { m.y[0], m.y[1], m.y[2], m.y[3]+v.y },
+      { m.z[0], m.z[1], m.z[2], m.z[3]+v.z },
+      { m.w[0], m.w[1], m.w[2], m.w[3]+v.w } };
+}
+
+fm2 scalefm2(fm2 m, fv2 v)
+{
+  return fm2
+    { { m.x[0]*v.x, m.x[1]     },
+      { m.y[0],     m.y[1]*v.y } };
+}
+dm2 scaledm2(dm2 m, dv2 v)
+{
+  return dm2
+    { { m.x[0]*v.x, m.x[1]     },
+      { m.y[0]    , m.y[1]*v.y } };
+}
+fm3 scalefm3(fm3 m, fv3 v)
+{
+  return fm3
+    { { m.x[0]*v.x, m.x[1]    , m.x[2]     },
+      { m.y[0]    , m.y[1]*v.y, m.y[2]     },
+      { m.z[0]    , m.z[1]    , m.z[2]*v.z } };
+}
+dm3 scaledm3(dm3 m, dv3 v)
+{
+  return dm3
+    { { m.x[0]*v.x, m.x[1]    , m.x[2]     },
+      { m.y[0]    , m.y[1]*v.y, m.y[2]     },
+      { m.z[0]    , m.z[1]    , m.z[2]*v.z } };
+}
+fm4 scalefm4(fm4 m, fv4 v)
+{
+  return fm4
+    { { m.x[0]*v.x, m.x[1]    , m.x[2]    , m.x[3]     },
+      { m.y[0]    , m.y[1]*v.y, m.y[2]    , m.y[3]     },
+      { m.z[0]    , m.z[1]    , m.z[2]*v.z, m.z[3]     },
+      { m.w[0]    , m.w[1]    , m.w[2]    , m.w[3]*v.w } };
+}
+dm4 scaledm4(dm4 m, dv4 v)
+{
+  return dm4
+    { { m.x[0]*v.x, m.x[1]    , m.x[2]    , m.x[3]     },
+      { m.y[0]    , m.y[1]*v.y, m.y[2]    , m.y[3]     },
+      { m.z[0]    , m.z[1]    , m.z[2]*v.z, m.z[3]     },
+      { m.w[0]    , m.w[1]    , m.w[2]    , m.w[3]*v.w } };
+}
+
+fm2 rotatefm2(float t)
+{
+  float cost = cos(t);
+  float sint = sin(t);
+  return fm2
+    { { cost, -sint },
+      { sint, cost } };
+}
+dm2 rotatedm2(double t)
+{
+  float cost = cos(t);
+  float sint = sin(t);
+  return dm2
+    { { cost, -sint },
+      { sint, cost } };
+}
+fm3 rotatefm3(fv3 v, float t)
+{
+  float cost = cos(t);
+  float oneminuscost = 1.f - cost;
+  float sint = sin(t);
+  float xx = v.x*v.x;
+  float xy = v.x*v.y;
+  float xz = v.x*v.z;
+  float yy = v.y*v.y;
+  float yz = v.y*v.z;
+  float zz = v.z*v.z;
+  return fm3
+    { {     (cost)+(xx*oneminuscost), (xy*oneminuscost)-(v.z*sint), (xz*oneminuscost)+(v.y*sint) },
+      { (xy*oneminuscost)+(v.z*sint),     (cost)+(yy*oneminuscost), (yz*oneminuscost)-(v.x*sint) },
+      { (xz*oneminuscost)-(v.y*sint), (yz*oneminuscost)+(v.x*sint),     (cost)+(zz*oneminuscost) } };
+}
+dm3 rotatedm3(dv3 v, double t)
+{
+  double cost = cos(t);
+  double oneminuscost = 1. - cost;
+  double sint = sin(t);
+  double xx = v.x*v.x;
+  double xy = v.x*v.y;
+  double xz = v.x*v.z;
+  double yy = v.y*v.y;
+  double yz = v.y*v.z;
+  double zz = v.z*v.z;
+  return dm3
+    { {     (cost)+(xx*oneminuscost), (xy*oneminuscost)-(v.z*sint), (xz*oneminuscost)+(v.y*sint) },
+      { (xy*oneminuscost)+(v.z*sint),     (cost)+(yy*oneminuscost), (yz*oneminuscost)-(v.x*sint) },
+      { (xz*oneminuscost)-(v.y*sint), (yz*oneminuscost)+(v.x*sint),     (cost)+(zz*oneminuscost) } };
+}
+fm4 rotate3dfm4(fv3 v, float t)
+{
+  float cost = cos(t);
+  float oneminuscost = 1.f - cost;
+  float sint = sin(t);
+  float xx = v.x*v.x;
+  float xy = v.x*v.y;
+  float xz = v.x*v.z;
+  float yy = v.y*v.y;
+  float yz = v.y*v.z;
+  float zz = v.z*v.z;
+  return fm4
+    { {     (cost)+(xx*oneminuscost), (xy*oneminuscost)-(v.z*sint), (xz*oneminuscost)+(v.y*sint), 0.f },
+      { (xy*oneminuscost)+(v.z*sint),     (cost)+(yy*oneminuscost), (yz*oneminuscost)-(v.x*sint), 0.f },
+      { (xz*oneminuscost)-(v.y*sint), (yz*oneminuscost)+(v.x*sint),     (cost)+(zz*oneminuscost), 0.f },
+      {                          0.f,                          0.f,                          0.f, 1.f } };
+}
+dm4 rotate3ddm4(dv3 v, double t)
+{
+  double cost = cos(t);
+  double oneminuscost = 1. - cost;
+  double sint = sin(t);
+  double xx = v.x*v.x;
+  double xy = v.x*v.y;
+  double xz = v.x*v.z;
+  double yy = v.y*v.y;
+  double yz = v.y*v.z;
+  double zz = v.z*v.z;
+  return dm4
+    { {     (cost)+(xx*oneminuscost), (xy*oneminuscost)-(v.z*sint), (xz*oneminuscost)+(v.y*sint), 0. },
+      { (xy*oneminuscost)+(v.z*sint),     (cost)+(yy*oneminuscost), (yz*oneminuscost)-(v.x*sint), 0. },
+      { (xz*oneminuscost)-(v.y*sint), (yz*oneminuscost)+(v.x*sint),     (cost)+(zz*oneminuscost), 0. },
+      {                           0.,                           0.,                           0., 1. } };
 }
 
 #endif
